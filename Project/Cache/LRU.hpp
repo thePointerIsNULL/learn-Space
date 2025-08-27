b41e93c854a514580d38ac1f42071b508d05a8fc
Cao <deadout@126.com>
2025-08-27 18:01:00 +0800

    代码

--- Project/Cache/LRU.hpp
+++ Project/Cache/LRU.hpp
@@ -7,8 +7,12 @@
 */
 #pragma once
 #include <unordered_map>
+#include <memory>
 using uint = unsigned int;
 
+//循环双链表+hashMap
+//热节点会在链表表头
+//超过容量时会移除链表尾部的节点
 template <typename Key, typename Value>
 class LRUContainers
 {
@@ -17,17 +21,28 @@ public:
 	struct LRUNode
 	{
 		Key key_;
-		value value_;
+		Value value_;
 		LRUNode* preNode_ = nullptr;
 		LRUNode* nextNode_ = nullptr;
+		LRUNode(Key&& key, Value&& value) :key_(std::move(key)), value_(std::move(value)) {};
+		LRUNode(const Key& key,const Value&& value) :key_(key ), value_(value) {};
 	};
-	LRUContainers() = default;
+	LRUContainers(uint size);
 	~LRUContainers();
 private:
 	NodeMap m_nodeMap;
 	LRUNode* m_sentryNode = nullptr;
 	uint m_size = 0;
 };
+
+template <typename Key, typename Value>
+LRUContainers<Key, Value>::LRUContainers(uint size)
+	:m_size(size)
+{
+	m_sentryNode = new LRUNode({}, {});
+	m_sentryNode->preNode_ = m_sentryNode;
+	m_sentryNode->nextNode_ = m_sentryNode;
+}
 
 template <typename Key, typename Value>
 LRUContainers<Key, Value>::~LRUContainers()
--- Project/Cache/main.cpp
+++ Project/Cache/main.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
-
+#include<string>
+#include "LRU.hpp"
 int main(int argc, char** argv)
 {
+
+	LRUContainers<int, std::string> a;
+
 	return 0;
 }
