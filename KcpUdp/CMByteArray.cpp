d7fb12b5bb7e3c491801a1064ebd2010239aa223
homeDesktopPC <caoliguo1999@gmail.com>
2025-10-14 00:03:09 +0800

    更新

---
+++ KcpUdp/CMByteArray.cpp
@@ -0,0 +1,291 @@
+#include "CMByteArray.h"
+#include <stdexcept>
+#include <string.h>
+
+size_t CMByteArray::_defInitcapacity = 1024;
+double CMByteArray::_step = 1.5;
+#define _assert(a) if (!(a))throw std::out_of_range("CMByteArray:Index out of range!");
+
+CMByteArray::CMByteArray() noexcept
+	:_capacity(_defInitcapacity)
+	, _size(0)
+{
+	resetBuffer();
+}
+
+CMByteArray::CMByteArray(const char* str) noexcept
+{
+	size_t size = strlen(str);
+	_capacity = size;
+	_size = 0;
+	resetBuffer();
+	insertElement(0, str, size);
+}
+
+CMByteArray::CMByteArray(size_t capacity) noexcept
+	:_capacity(capacity)
+	, _size(0)
+{
+	resetBuffer();
+}
+
+CMByteArray::CMByteArray(const CMByteArray& other) noexcept
+{
+	_data = other._data;
+	_size = other._size;
+	_capacity = other._capacity;
+}
+
+CMByteArray::CMByteArray(CMByteArray&& other) noexcept
+{
+	_data = other._data;
+	_size = other._size;
+	_capacity = other._capacity;
+
+	other._data.reset();
+	other._size = 0;
+	other._capacity = 0;
+}
+
+size_t CMByteArray::size() const
+{
+	return _size;
+}
+
+
+size_t CMByteArray::capacity() const
+{
+	return _capacity;
+}
+
+CMByteArray& CMByteArray::append(const char* str)
+{
+	insertElement(_size, str, strlen(str));
+	return *this;
+}
+
+CMByteArray& CMByteArray::append(const char* byte, size_t len)
+{
+	insertElement(_size, byte, len);
+	return *this;
+}
+
+CMByteArray& CMByteArray::append(const CMByteArray& str)
+{
+	insertElement(_size, str._data.get(), str._size);
+	return *this;
+}
+
+CMByteArray& CMByteArray::insert(size_t pos, const char* byte, size_t len)
+{
+	insertElement(pos, byte, len);
+	return *this;
+}
+
+CMByteArray& CMByteArray::insert(size_t pos, const char* str)
+{
+	insertElement(pos, str, strlen(str));
+	return *this;
+}
+
+CMByteArray& CMByteArray::insert(size_t pos, const CMByteArray& str)
+{
+	insertElement(pos, str._data.get(), str._size);
+	return *this;
+}
+
+CMByteArray& CMByteArray::prepend(const char* str)
+{
+	insertElement(0, str, strlen(str));
+	return *this;
+}
+
+
+CMByteArray& CMByteArray::prepend(const char* byte, size_t len)
+{
+	insertElement(0, byte, len);
+	return *this;
+}
+
+CMByteArray& CMByteArray::prepend(const CMByteArray& str)
+{
+	insertElement(0, str._data.get(), str._size);
+	return *this;
+}
+
+CMByteArray& CMByteArray::remove(size_t pos, size_t len)
+{
+	reomveElement(pos, len);
+	return*this;
+}
+
+void CMByteArray::detach()
+{
+	if (!_data.unique())
+	{
+		resetBuffer();
+	}
+}
+
+void CMByteArray::clear()
+{
+	_size = 0;
+	_capacity = _defInitcapacity;
+	resetBuffer();
+}
+
+void CMByteArray::reserve(size_t size)
+{
+	_capacity = size;
+	_size = 0;
+	resetBuffer();
+}
+
+void CMByteArray::squeeze()
+{
+	_capacity = _size;
+	resetBuffer();
+}
+
+char CMByteArray::front() const
+{
+	return at(0);
+}
+
+char CMByteArray::back() const
+{
+	return at(_size - 1);
+}
+
+char CMByteArray::at(size_t pos)const
+{
+	_assert(pos < _size);
+	return _data.get()[pos];
+}
+
+char& CMByteArray::operator[](size_t pos)
+{
+	_assert(pos < _size);
+	return _data.get()[pos];
+}
+
+CMByteArray::Iterator CMByteArray::begin()
+{
+	return Iterator(_data.get());
+}
+
+CMByteArray::IteratorConst CMByteArray::begin() const
+{
+	return IteratorConst(_data.get());
+}
+
+CMByteArray::Iterator CMByteArray::end()
+{
+	return Iterator(_data.get() + _size);
+}
+
+CMByteArray::IteratorConst CMByteArray::end() const
+{
+	return IteratorConst(_data.get() + _size);
+}
+
+const char* CMByteArray::data() const
+{
+	return _data.get();
+}
+
+char* CMByteArray::data()
+{
+	return _data.get();
+}
+
+const char* CMByteArray::constData() const
+{
+	return _data.get();
+}
+
+CMByteArray& CMByteArray::operator=(CMByteArray&& other) noexcept
+{
+	_data = other._data;
+	_size = other._size;
+	_capacity = other._capacity;
+
+	other._data.reset();
+	other._size = 0;
+	other._capacity = 0;
+
+	return *this;
+}
+
+CMByteArray& CMByteArray::operator=(const CMByteArray& other) noexcept
+{
+	_data = other._data;
+	_size = other._size;
+	_capacity = other._capacity;
+	return *this;
+}
+
+CMByteArray::~CMByteArray() noexcept
+{
+
+}
+
+
+CMByteArray::CMByteArray(const char* str, size_t len) noexcept
+	:_size(len), _capacity(len)
+{
+	resetBuffer();
+	insertElement(0, str, len);
+}
+
+
+
+
+void CMByteArray::resetBuffer()
+{
+	std::shared_ptr<char> buffer(new char[_capacity] {0}, std::default_delete<char[]>());
+	memcpy(buffer.get(), _data.get(), _size);
+	_data = buffer;
+}
+
+void CMByteArray::insertElement(size_t pos, const char* dataP, size_t dataLen)
+{
+	_assert(pos <= _size);
+
+	size_t size = _size + dataLen;
+	size_t capacity = _capacity;
+	if (size > _capacity)
+	{
+		capacity = static_cast<size_t>((_capacity + dataLen) * _step);
+	}
+
+	if (!_data.unique() || capacity > _capacity)
+	{
+		_capacity = capacity;
+		resetBuffer();
+	}
+
+	memcpy(_data.get() + dataLen, _data.get() + pos, _size - pos);
+	memcpy(_data.get() + pos, dataP, dataLen);
+
+	_size = size;
+}
+
+void CMByteArray::reomveElement(size_t pos, size_t len)
+{
+	_assert(pos <= _size);
+	if (!_data.unique())
+	{
+		resetBuffer();
+	}
+	if (_size - pos < len)
+	{
+		len = _size - pos;
+	}
+	size_t moveSize = _size - pos - len;
+	memcpy(_data.get() + pos, _data.get() + pos + len, moveSize);
+	_size -= len;
+	memset(_data.get() + _size, 0, _capacity - _size);
+}
+
+
--- KcpUdp/main.cpp
+++ KcpUdp/main.cpp
@@ -3,15 +3,56 @@
 #include <arpa/inet.h>
 #include "trace.h"
 #include <string.h>
+#include <chrono>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "CMByteArray.h"
+
 struct User
 {
 	unsigned int coveKey = 0;
 	sockaddr_in addr;
+	int fd = -1;
 };
+
+int updSend(const char* buf, int len, struct IKCPCB* kcp, void* user)
+{
+	User* userP = reinterpret_cast<User*>(user);
+
+	return ::sendto(userP->fd, buf, len, 0, reinterpret_cast<sockaddr*>(&userP->addr), sizeof(sockaddr_in));
+}
+
+unsigned int getCurrentTime()
+{
+	auto now = std::chrono::high_resolution_clock::now();
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+		now.time_since_epoch()
+	);
+	return ms.count();
+}
+
+bool isSameAddr(sockaddr_in* a, sockaddr_in* b)
+{
+	return a->sin_addr.s_addr == b->sin_addr.s_addr
+		&& a->sin_port == b->sin_port;
+}
+
+bool vaildRecvLen(int len)
+{
+	if (len < 0)
+	{
+		return false;
+	}
+	else if (len < 24)//kcp头部的大小
+	{
+		return false;
+	}
+}
 
 int main(int argc, char** argv)
 {
-	if (argc < 4)
+	if (argc < 5)
 	{
 		TRACE("wrong argc:", argc);
 		return -1;
@@ -20,21 +61,78 @@ int main(int argc, char** argv)
 	short port = static_cast<unsigned int>(atoi(argv[2]));
 	unsigned int coveKey = static_cast<unsigned int>(atoi(argv[3]));
 
+	CMByteArray msg = argv[4];
+
 	User user;
 	user.coveKey = coveKey;
 
-	sockaddr_in addr{ 0 };
+	user.addr.sin_family = AF_INET;
+	::inet_aton(ip, &user.addr.sin_addr);
+	user.addr.sin_port = htons(port);
 
-	memcpy(&user.addr, &addr, sizeof(sockaddr_in));
+	user.fd = ::socket(AF_INET, SOCK_DGRAM, 0);
+	int optVal = ::fcntl(user.fd, F_GETFL, 0);
+	optVal |= O_NONBLOCK;
+	::fcntl(user.fd, F_SETFL, optVal);
 
-	ikcp_create(coveKey, &addr);
+	sockaddr_in localAddr{ 0 };
+	localAddr.sin_family = AF_INET;
+	localAddr.sin_addr.s_addr = 0;
+	localAddr.sin_port = htons(port);
+	::bind(user.fd, reinterpret_cast<sockaddr*>(&localAddr), sizeof(sockaddr_in));
 
+	ikcpcb* kcpcb = ikcp_create(coveKey, &user);
+	kcpcb->output = &updSend;
+
+	ikcp_nodelay(kcpcb, 0, 10, 2, 0);
+
+	char buffer[1024]{ 0 };
 	while (true)
 	{
+		ikcp_update(kcpcb, getCurrentTime());
 
+		CMByteArray udpMsg;
+		while (true)
+		{
+			memset(buffer, 0, 1024);
+			sockaddr_in fromAddr{ 0 };
+			socklen_t addrLen = sizeof(sockaddr_in);
+
+			int recvRet = ::recvfrom(user.fd, buffer, 1024, 0, reinterpret_cast<sockaddr*>(&fromAddr), &addrLen);
+			if (recvRet < 0
+				|| !isSameAddr(&fromAddr, &user.addr))
+			{
+				break;
+			}
+			udpMsg.append(buffer, recvRet);
+		}
+
+		if (!udpMsg.isEmpty())
+		{
+			ikcp_input(kcpcb, udpMsg.data(), udpMsg.size());
+		}
+
+		CMByteArray recvMsg;
+		while (true)
+		{
+			memset(buffer, 0, 1024);
+			int recvRet = ikcp_recv(kcpcb, buffer, 1024);
+			if (recvRet < 0)
+			{
+				break;
+			}
+			recvMsg.append(buffer, recvRet);
+		}
+		if (!recvMsg.isEmpty())
+		{
+			std::cout << recvMsg.constData() << '\n';
+		}
+
+		ikcp_send(kcpcb, msg.constData(), msg.size());
+
+		usleep(10);
+		//ikcp_check(kcpcb, getCurrentTime());
 	}
 
-
-	printf("%s 向你问好!\n", "KcpUdp");
 	return 0;
 }
