7f0f30e1f69b7c1be57489f4c91bf1afe9b115e6
Cao <deadout@126.com>
2025-11-20 15:42:37 +0800

    1

--- TCP/tcp.c
+++ TCP/tcp.c
@@ -3,6 +3,9 @@
 #include <rte_malloc.h>
 
 static TCBNode* TCBList = NULL;
+static char FDSET[FDSetSize];
+
+
 static int isSameAddr(AddrGrop* a, AddrGrop* b)
 {
 	return (a->srcIp == b->srcIp && a->desIp == b->desIp && a->srcPort == b->srcPort && a->desPort == b->desPort) ? 0 : -1;
@@ -37,6 +40,48 @@ static void delTCBNode(TCBNode* node)
 	rte_free(node);
 }
 
+static int getFD()
+{
+	for (uint32_t i = 0; i < FDCOUNT; i++)
+	{
+		uint32_t index = i / 8;
+		uint32_t pos = i % 8;
+		if (FDSET[index] & (1 << pos) == 0)
+		{
+			FDSET[index] = FDSET[index] | (1 << pos);
+			return i;
+		}
+	}
+}
+
+static void resetFD(int fd)
+{
+	uint32_t index = fd / 8;
+	uint32_t pos = fd % 8;
+
+	if (index >= FDSetSize)
+	{
+		return;
+	}
+	FDSET[index] = FDSET[index] ^ (1 << pos);
+}
+
+
+void initTCP()
+{
+	if (TCBList == NULL)
+	{
+		TCBList = rte_malloc("tcbList", sizeof(TCBNode), 0);
+	}
+
+	memset(FDSET, 0, FDSetSize);
+}
+
+void closeTCP()
+{
+
+
+}
 
 void tcp(uint32_t srcIp, uint32_t desIp, TCPHDR* tcpHdr)
 {
@@ -46,19 +91,28 @@ void tcp(uint32_t srcIp, uint32_t desIp, TCPHDR* tcpHdr)
 	addrGrop.srcPort = tcpHdr->src_port;
 	addrGrop.desPort = tcpHdr->dst_port;
 
-	if (TCBList == NULL)
-	{
-		TCBList = rte_malloc("tcbList", sizeof(TCBNode), 0);
-	}
-
 	TCB* tcb = findTCB(&addrGrop);
 
-	assert(tcb == NULL);
+	if (tcb == NULL)
+	{
+		return;//没有可通迅的TCB
+	}
+
+
+
+
+
+	/*if (tcb == NULL)
+	{
+		tcb = rte_malloc("tcb", sizeof(TCB), 0);
+		tcb->addrGrop = addrGrop;
+		tcb->state = TCPCLOSE;
+		tcb->fd
+	}*/
+
 
 
 }
-
-
 
 TCB* findTCB(AddrGrop* addr)
 {
@@ -79,3 +133,47 @@ TCB* findTCB(AddrGrop* addr)
 	return NULL;
 
 }
+TCB* findTCB(int fd)
+{
+	TCBNode* node = TCBList->nextTcb;
+	while (node != NULL)
+	{
+		TCB* tcb = node->tcb;
+		if (tcb->fd == fd)
+		{
+			return tcb;
+		}
+	}
+	return NULL;
+
+}
+
+
+int cmSocket()
+{
+	int fd = getFD();
+	TCB* tcb = rte_malloc("tcb", sizeof(TCB), 0);
+	memset(&tcb->addrGrop, 0, sizeof(AddrGrop));
+	tcb->fd = fd;
+	tcb->state = TCPCLOSE;
+
+	TCBNode* tcbNode = rte_malloc("tcbNode", sizeof(TCBNode), 0);
+	tcbNode->tcb = tcb;
+
+	addTCBNode(tcbNode);
+
+	return fd;
+}
+
+void cmClose(int fd)
+{
+	TCB* tcb = findTCB(fd);
+	if (tcb == NULL)
+	{
+		return;
+	}
+
+}
+
+
+
--- TCP/tcp.h
+++ TCP/tcp.h
@@ -1,16 +1,24 @@
 #pragma once
 #include <rte_tcp.h>
 
+#define TCPCLOSE 0
 #define TCPSYN_SENT 1
-#define TCPSYN_RECV 1 << 1
-#define TCPESTABLISHED 1 << 2
-#define TCPCLOSE_WAIT1 1 << 3
-#define TCPCLOSE_WAIT 1 << 4
-#define TCPCLOSE_WAIT2 1 << 5
-#define TCPTIME_WAIT 1 << 6
+#define TCPSYN_RECV 2
+#define TCPESTABLISHED 3
+#define TCPCLOSE_WAIT1 4
+#define TCPCLOSE_WAIT 5
+#define TCPCLOSE_WAIT2 6
+#define TCPTIME_WAIT 7
+#define TCPLAST_ACK 8
+#define TCPLISTEN 9
+#define TCPCLOSING 10
+
 
 #define WindowsSize  10480
 #define BuffSize  20480
+
+#define FDSetSize 20480
+#define FDCOUNT  FDSetSize * 8
 
 
 typedef struct rte_tcp_hdr TCPHDR;
@@ -44,7 +52,24 @@ typedef struct TCBNode
 	TCB* nextTcb;
 }TCBNode;
 
-void tcp(uint32_t srcIp, uint32_t desIp,TCPHDR * tcpHdr);
+void initTCP();
+void closeTCP();
+
+
+void tcp(uint32_t srcIp, uint32_t desIp, TCPHDR* tcpHdr);
+
+int cmSocket();
+void cmClose(int fd);
+bool cmBind(int fd, sockaddr_in* addr);
+bool cmListen(int fd);
+int cmAccept(int fd);
+bool cmConnect(int fd, sockaddr_in* addr);
+
+int cmSend(int fd, char* data, int size);
+int cmRead(int fd, char* buffer, int size);
+
+
+
 
 
 
